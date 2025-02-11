fab296d19afcfe7a23eddfef0dedfe10f0ee40c4
Hüseyin Güney <guneyin@gmail.com>
2025-02-11 16:58:10 +0300

    company service refactored

--- internal/service/company/company.go
+++ internal/service/company/company.go
@@ -6,7 +6,6 @@ import (
 	"github.com/guneyin/kapscan/internal/entity"
 	"github.com/guneyin/kapscan/internal/repo/company"
 	"github.com/guneyin/kapscan/util"
-	"github.com/vcraescu/go-paginator/v2"
 )
 
 type Service struct {
@@ -19,16 +18,8 @@ func NewService() *Service {
 	}
 }
 
-type ListGetter struct {
-	repo   *company.Repo
-	search string
-	offset int
-	limit  int
-	pg     paginator.Paginator
-}
-
-func (s *Service) Search(term string) *ListGetter {
-	return &ListGetter{
+func (s *Service) Search(term string) *Pager {
+	return &Pager{
 		repo:   s.repo,
 		search: term,
 		offset: -1,
@@ -58,51 +49,4 @@ func (s *Service) GetByCode(code string) (*dto.Company, error) {
 
 func (s *Service) Save(company *entity.Company) error {
 	return s.repo.Save(company)
-}
-
-func (lg *ListGetter) Offset(offset int) *ListGetter {
-	lg.offset = offset
-	return lg
-}
-
-func (lg *ListGetter) Limit(limit int) *ListGetter {
-	lg.limit = limit
-	return lg
-}
-
-func (lg *ListGetter) Do() (*ListGetter, error) {
-	pg, err := lg.repo.Search(lg.search, lg.offset, lg.limit)
-	if err != nil {
-		return nil, err
-	}
-	lg.pg = pg
-
-	return lg, nil
-}
-
-func (lg *ListGetter) PageData() paginator.Paginator {
-	return lg.pg
-}
-
-func (lg *ListGetter) Data() (*entity.CompanyList, error) {
-	data := &entity.CompanyList{}
-	err := lg.pg.Results(data)
-	if err != nil {
-		return nil, err
-	}
-
-	return data, nil
-}
-
-func (lg *ListGetter) DataAs(m any) error {
-	data, err := lg.Data()
-	if err != nil {
-		return err
-	}
-
-	_, err = util.Convert(data, m)
-	if err != nil {
-		return err
-	}
-	return nil
 }
---
+++ internal/service/company/company_pager.go
@@ -0,0 +1,63 @@
+package company
+
+import (
+	"github.com/guneyin/kapscan/internal/entity"
+	"github.com/guneyin/kapscan/internal/repo/company"
+	"github.com/guneyin/kapscan/util"
+	"github.com/vcraescu/go-paginator/v2"
+)
+
+type Pager struct {
+	repo   *company.Repo
+	search string
+	offset int
+	limit  int
+	pg     paginator.Paginator
+}
+
+func (lg *Pager) Offset(offset int) *Pager {
+	lg.offset = offset
+	return lg
+}
+
+func (lg *Pager) Limit(limit int) *Pager {
+	lg.limit = limit
+	return lg
+}
+
+func (lg *Pager) Do() (*Pager, error) {
+	pg, err := lg.repo.Search(lg.search, lg.offset, lg.limit)
+	if err != nil {
+		return nil, err
+	}
+	lg.pg = pg
+
+	return lg, nil
+}
+
+func (lg *Pager) PageData() paginator.Paginator {
+	return lg.pg
+}
+
+func (lg *Pager) Data() (*entity.CompanyList, error) {
+	data := &entity.CompanyList{}
+	err := lg.pg.Results(data)
+	if err != nil {
+		return nil, err
+	}
+
+	return data, nil
+}
+
+func (lg *Pager) DataAs(m any) error {
+	data, err := lg.Data()
+	if err != nil {
+		return err
+	}
+
+	_, err = util.Convert(data, m)
+	if err != nil {
+		return err
+	}
+	return nil
+}
